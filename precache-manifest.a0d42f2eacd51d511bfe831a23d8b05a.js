73ef2eb09ddbc2437a1a3b6feb7ec607ef9fe2ad
aran <othman.aran@gmail.com>
2019-12-21 22:58:25 +0300

    Updates

---
+++ precache-manifest.a0d42f2eacd51d511bfe831a23d8b05a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b557470694a3c9330e9fc71c16b3c06b",
+    "url": "/sulibb/index.html"
+  },
+  {
+    "revision": "f1a513b495105f958531",
+    "url": "/sulibb/static/css/2.374fe853.chunk.css"
+  },
+  {
+    "revision": "7b7e15b320e23ad5ef3f",
+    "url": "/sulibb/static/css/main.dcbacf4a.chunk.css"
+  },
+  {
+    "revision": "f1a513b495105f958531",
+    "url": "/sulibb/static/js/2.e9132278.chunk.js"
+  },
+  {
+    "revision": "7b7e15b320e23ad5ef3f",
+    "url": "/sulibb/static/js/main.1229cd0b.chunk.js"
+  },
+  {
+    "revision": "fad4543852ad21967817",
+    "url": "/sulibb/static/js/runtime-main.53228499.js"
+  },
+  {
+    "revision": "0b2c0e74f616b916c9ca2027f3ddc2f3",
+    "url": "/sulibb/static/media/DroidKufi-Regular.0b2c0e74.woff"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/sulibb/static/media/Roboto-Medium.58aef543.ttf"
+  }
+]);
